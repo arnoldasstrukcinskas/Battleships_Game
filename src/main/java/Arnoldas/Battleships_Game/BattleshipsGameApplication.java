de37a0ef295a50e77202069a2587e37df7a47349
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-12-27 15:08:26 +0200

    Git init

---
+++ src/main/java/Arnoldas/Battleships_Game/BattleshipsGameApplication.java
@@ -0,0 +1,13 @@
+package Arnoldas.Battleships_Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipsGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Arnoldas/Battleships_Game/BattleshipsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package Arnoldas.Battleships_Game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
