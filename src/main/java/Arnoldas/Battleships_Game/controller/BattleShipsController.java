551b8779f8e8bbb2752eda3ec87159a3aefbfc3a
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-12-28 17:21:47 +0200

    Modify controller for creating new separate fields

--- src/main/java/Arnoldas/Battleships_Game/controller/BattleShipsController.java
+++ src/main/java/Arnoldas/Battleships_Game/controller/BattleShipsController.java
@@ -1,11 +1,12 @@
 package Arnoldas.Battleships_Game.controller;
 
+import Arnoldas.Battleships_Game.repository.model.GameBoard;
 import Arnoldas.Battleships_Game.service.BattleShipsService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/battleShips")
@@ -14,11 +15,14 @@ public class BattleShipsController {
 @Autowired
 BattleShipsService battleShipsService;
 
-// http://localhost:8080/battleShips/home
-@GetMapping(value = "/home")
-public String home(HttpSession session){
 
-    battleShipsService.paintShips();
+// http://localhost:8080/battleShips/home
+@RequestMapping(value = "/home", method = RequestMethod.GET)
+public String getNewBoard(Model model){
+
+    GameBoard gameBoard = new GameBoard();
+    char[][] field = gameBoard.getField();
+    battleShipsService.paintShips(field);
     return "home";
 }
 }
