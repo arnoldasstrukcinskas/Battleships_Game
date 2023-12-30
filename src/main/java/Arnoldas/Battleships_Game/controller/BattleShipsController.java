735fb5f2ac385d3142d021be053cd2081028299f
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-12-30 12:21:21 +0200

    Send game Board to front-end

--- src/main/java/Arnoldas/Battleships_Game/controller/BattleShipsController.java
+++ src/main/java/Arnoldas/Battleships_Game/controller/BattleShipsController.java
@@ -2,11 +2,12 @@ package Arnoldas.Battleships_Game.controller;
 
 import Arnoldas.Battleships_Game.repository.model.GameBoard;
 import Arnoldas.Battleships_Game.service.BattleShipsService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/battleShips")
@@ -15,14 +16,20 @@ public class BattleShipsController {
 @Autowired
 BattleShipsService battleShipsService;
 
-
-// http://localhost:8080/battleShips/home
+    //    http://localhost:8080/battleShips/home
 @RequestMapping(value = "/home", method = RequestMethod.GET)
-public String getNewBoard(Model model){
-
-    GameBoard gameBoard = new GameBoard();
-    char[][] field = gameBoard.getField();
-    battleShipsService.paintShips(field);
+    public String home(){
     return "home";
 }
+
+    //    http://localhost:8080/battleShips/generateField
+    @RequestMapping(value = "/generateField", method = RequestMethod.POST)
+    @ResponseBody
+    public char[][] generateField(HttpSession session) {
+        GameBoard gameBoard = new GameBoard();
+        char[][] generatedBoard = battleShipsService.paintShips(gameBoard.getField());
+        session.setAttribute("gameBoard", generatedBoard);
+        return generatedBoard;
+    }
+
 }
