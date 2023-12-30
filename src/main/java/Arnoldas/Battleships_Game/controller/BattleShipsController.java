67cbf9f5c5d5901fea6f90edfb812484d82ea382
arnoldasstrukcinskas <141561525+arnoldasstrukcinskas@users.noreply.github.com>
2023-12-30 19:36:14 +0200

    Edit controller for printing board and making moves

--- src/main/java/Arnoldas/Battleships_Game/controller/BattleShipsController.java
+++ src/main/java/Arnoldas/Battleships_Game/controller/BattleShipsController.java
@@ -2,12 +2,11 @@ package Arnoldas.Battleships_Game.controller;
 
 import Arnoldas.Battleships_Game.repository.model.GameBoard;
 import Arnoldas.Battleships_Game.service.BattleShipsService;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/battleShips")
@@ -15,21 +14,25 @@ public class BattleShipsController {
 
 @Autowired
 BattleShipsService battleShipsService;
+    GameBoard gameBoard;
 
     //    http://localhost:8080/battleShips/home
 @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String home(){
+    gameBoard = new GameBoard();
+    gameBoard.setField(battleShipsService.paintShips(gameBoard.getField()));
     return "home";
 }
 
-    //    http://localhost:8080/battleShips/generateField
-    @RequestMapping(value = "/generateField", method = RequestMethod.POST)
+    @RequestMapping(value = "/getMoveCords", method = RequestMethod.POST, consumes = "application/json")
     @ResponseBody
-    public char[][] generateField(HttpSession session) {
-        GameBoard gameBoard = new GameBoard();
-        char[][] generatedBoard = battleShipsService.paintShips(gameBoard.getField());
-        session.setAttribute("gameBoard", generatedBoard);
-        return generatedBoard;
+    public void getMoveCoordinates(@RequestBody ObjectNode cordsResponse) {
+
+    char[][] generatedBoard = gameBoard.getField();
+        System.out.println(cordsResponse);
+
+        battleShipsService.makeAMove(generatedBoard, cordsResponse);
+        //cia ikelti modifikatiota metoda, ir is cia imesti i html
     }
 
 }
